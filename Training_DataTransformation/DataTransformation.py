95a4375f8e95c9318a95b17e8ff6e5978422c63f
mohammadusama2 <mohammadusama2@hotmail.com>
2022-04-26 17:05:23 +0530

    Training Data Validation files added

---
+++ Training_DataTransformation/DataTransformation.py
@@ -0,0 +1,59 @@
+from cmath import log
+from datetime import datetime
+from os import listdir
+import pandas as pd
+from Application_Logging.logger import App_Logger
+
+class dataTransform:
+
+    """
+    We don't need any Data Transformation in this project during Training as we haven't found
+    any requirement to do so.
+    """
+
+
+
+    def __init__ (self):
+        self.goodDataPAth = "Training_Raw_Files_Validated/Good_Raw"
+        self.logger = App_Logger()
+
+
+    def renameColumns(self):
+
+
+        """
+        Description: This function renamed the columns having longer name with spaces in between them
+                     to shorter name and then replaces the original file with file having renamed columns.
+        Output: None
+        On Failue: Raise Exception
+        """
+
+        log_file = open("Training_Logs/renameColumns.txt", 'a+')
+        
+        try:
+            onlyfiles = [f for f in listdir(self.goodDataPAth)]
+            for file in onlyfiles:
+                data = pd.read_csv(self.goodDataPath+"/" + file)
+                data=data.rename(columns= lambda x: x.split(" ")[0])
+
+                data.to_excel(self.goodDataPath+"/" + file, index=False, header=True)
+                self.logger.log(log_file, "%s: Columns of file have been renamed to shorter name successfully!!" % file)
+     
+        except Exception as e:
+            self.logger.log(log_file, "Renaming of columns failed because:: %s" %e)
+            log_file.close()
+            raise e
+
+        log_file.close()    
+
+
+
+"""
+    def replaceMissingWithNull(self):
+
+        """
+        As there are no missing values we have found in any of the columns during EDA
+        so we don't need this function to be used in this project.
+        """
+        pass 
+    """
