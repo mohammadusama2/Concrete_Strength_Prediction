1c9d99b9fb81f417808813efa80751e2d9ffbd35
mohammadusama2 <mohammadusama2@hotmail.com>
2022-04-30 19:58:19 +0530

    updated concrete srength project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="concrete-strength-prediction",
+    version="0.0.1",
+    description="ML Regression Project",
+    author="Mohd Usama",
+    packages=find_packages(),
+    license="MIT"
+    )
+
